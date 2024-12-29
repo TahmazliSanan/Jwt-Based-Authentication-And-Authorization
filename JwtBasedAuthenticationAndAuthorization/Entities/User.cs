be9c6596ea2cd03b980eb9222953c8c7429bf0b5
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 03:03:15 +0400

    Create entity: User

---
+++ JwtBasedAuthenticationAndAuthorization/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace JwtBasedAuthenticationAndAuthorization.Entities
+{
+    public class User : BaseEntity<long>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime? BirthDateTime { get; set; }
+        public List<string> Roles { get; set; } = new();
+    }
+}
