3e8a6ffda3f32f0bfefc92df2c849995ef8f4ddb
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 01:59:41 +0400

    Create payload: BookResponse

---
+++ JwtBasedAuthenticationAndAuthorization/Payloads/BookResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtBasedAuthenticationAndAuthorization.Payloads
+{
+    public class BookResponse
+    {
+        public string Message { get; set; } = string.Empty;
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public DateTime? PublishedDate { get; set; }
+    }
+}
