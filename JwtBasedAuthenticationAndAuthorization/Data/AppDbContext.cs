76f2089fce54ea2fdaa10247542aed7470b599f4
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-29 22:27:34 +0400

    Create AppDbContext

---
+++ JwtBasedAuthenticationAndAuthorization/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtBasedAuthenticationAndAuthorization.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Entities.Book> Books { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
