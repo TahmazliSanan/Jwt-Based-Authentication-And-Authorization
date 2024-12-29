310d49b76f25ada2f93ee049e72eab523fbf3975
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-29 22:25:03 +0400

    Create config file and implement Fluent API

---
+++ JwtBasedAuthenticationAndAuthorization/EntityConfigs/BookConfig.cs
@@ -0,0 +1,21 @@
+﻿using JwtBasedAuthenticationAndAuthorization.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JwtBasedAuthenticationAndAuthorization.EntityConfigs
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("tbl_books");
+            builder.HasKey(b => b.Id);
+            builder.HasIndex(b => b.Name);
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(b => b.Price).IsRequired();
+            builder.Property(b => b.PublishedDate).IsRequired(false);
+        }
+    }
+}
