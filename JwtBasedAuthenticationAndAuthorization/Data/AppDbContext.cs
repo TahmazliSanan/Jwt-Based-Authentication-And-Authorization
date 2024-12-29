391932e40344e6576f4d264129f0ea767db9485d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-29 22:28:21 +0400

    Modifying...

--- JwtBasedAuthenticationAndAuthorization/Data/AppDbContext.cs
+++ JwtBasedAuthenticationAndAuthorization/Data/AppDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using System.Reflection;
+using JwtBasedAuthenticationAndAuthorization.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace JwtBasedAuthenticationAndAuthorization.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Entities.Book> Books { get; set; }
+        public DbSet<Book> Books { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
