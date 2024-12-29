03d5b5ed8fe38699dcfa326da0f635eb6f60f8e6
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-29 22:17:33 +0400

    Create entity: Book

---
+++ JwtBasedAuthenticationAndAuthorization/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtBasedAuthenticationAndAuthorization.Entities
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public DateTime? PublishedDate { get; set; }
+    }
+}
