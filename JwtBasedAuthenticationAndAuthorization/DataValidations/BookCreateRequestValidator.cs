c19c42591c88f314004c3d6ffb502f489d7d8ea8
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-29 22:50:25 +0400

    Create validator: BookCreateRequestValidator

---
+++ JwtBasedAuthenticationAndAuthorization/DataValidations/BookCreateRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using JwtBasedAuthenticationAndAuthorization.Payloads;
+
+namespace JwtBasedAuthenticationAndAuthorization.DataValidations
+{
+    public class BookCreateRequestValidator : AbstractValidator<BookCreateRequest>
+    {
+        public BookCreateRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+            
+            RuleFor(x => x.Price)
+                .InclusiveBetween(5, 100)
+                .WithMessage("Price must be between 5 and 100");
+        }
+    }
+}
