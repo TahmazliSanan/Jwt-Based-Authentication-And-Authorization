3a71fccf33ddfd995c07868923347caecf00dd38
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 11:20:05 +0400

    Create validator: LoginRequestValidator

---
+++ JwtBasedAuthenticationAndAuthorization/DataValidations/Login/LoginRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using JwtBasedAuthenticationAndAuthorization.Payloads.Login;
+
+namespace JwtBasedAuthenticationAndAuthorization.DataValidations.Login
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .MaximumLength(100)
+                .WithMessage("Maximum length cannot be greater than 100");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .MinimumLength(8)
+                .WithMessage("Minimum length cannot be less than 8")
+                .MaximumLength(50)
+                .WithMessage("Maximum length cannot be greater than 50");
+        }
+    }
+}
