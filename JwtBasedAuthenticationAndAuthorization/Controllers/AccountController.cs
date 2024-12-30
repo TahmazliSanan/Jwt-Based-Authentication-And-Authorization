24e6ffb6ae57c9d57346c35f2d210a1799e43119
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 12:35:20 +0400

    Complete Login method

--- JwtBasedAuthenticationAndAuthorization/Controllers/AccountController.cs
+++ JwtBasedAuthenticationAndAuthorization/Controllers/AccountController.cs
@@ -1,8 +1,13 @@
-﻿using JwtBasedAuthenticationAndAuthorization.Data;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtBasedAuthenticationAndAuthorization.Data;
 using JwtBasedAuthenticationAndAuthorization.Entities;
+using JwtBasedAuthenticationAndAuthorization.Payloads.Login;
 using JwtBasedAuthenticationAndAuthorization.Payloads.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtBasedAuthenticationAndAuthorization.Controllers
 {
@@ -10,10 +15,12 @@ namespace JwtBasedAuthenticationAndAuthorization.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
+        private readonly IConfiguration _config;
         private readonly AppDbContext _context;
 
-        public AccountController(AppDbContext context)
+        public AccountController(IConfiguration config, AppDbContext context)
         {
+            _config = config;
             _context = context;
         }
 
@@ -68,9 +75,68 @@ namespace JwtBasedAuthenticationAndAuthorization.Controllers
 
         [HttpPost]
         [Route("/login")]
-        public IActionResult Login()
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
         {
-            return Ok();
+            LoginResponse loginResponse;
+            var foundUser = await _context
+                .Users
+                .FirstOrDefaultAsync(u => u.Email.ToLower().Equals(loginRequest.Email.ToLower()) && u.Password.Equals(loginRequest.Password));
+
+            if (foundUser is null)
+            {
+                loginResponse = new LoginResponse
+                {
+                    Message = "Invalid email or password"
+                };
+            }
+            else
+            {
+                loginResponse = new LoginResponse
+                {
+                    Message = "Login successful",
+                    UserId = foundUser.Id,
+                    Jwt = GenerateJwt(foundUser)
+                };
+            }
+
+            return Ok(loginResponse);
+        }
+
+        private string GenerateJwt(User user)
+        {
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var signingKey = _config.GetSection("Jwt").GetSection("SigningKey").Value;
+            var signingKeyAsBytes = Encoding.UTF8.GetBytes(signingKey);
+
+            var claimList = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new("FirstName", user.FirstName),
+                new("LastName", user.LastName),
+                new(ClaimTypes.Email, user.Email)
+            };
+
+            if (user.BirthDateTime.HasValue)
+            {
+                claimList.Add(new Claim("BirthDateTime", user.BirthDateTime.Value.ToString("O")));
+            }
+
+            claimList.AddRange(user.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claimList),
+                Issuer = "example.com",
+                Audience = "example.com",
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(signingKeyAsBytes),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var createdToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var jwt = jwtSecurityTokenHandler.WriteToken(createdToken);
+            return jwt;
         }
     }
 }
