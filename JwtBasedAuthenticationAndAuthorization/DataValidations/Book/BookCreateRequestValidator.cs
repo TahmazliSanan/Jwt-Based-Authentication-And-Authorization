5211b5bffe98a6e8b9a99164dd9c26397c574dd9
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 10:21:21 +0400

    Change namespace

---
+++ JwtBasedAuthenticationAndAuthorization/DataValidations/Book/BookCreateRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using JwtBasedAuthenticationAndAuthorization.Payloads.Book;
+
+namespace JwtBasedAuthenticationAndAuthorization.DataValidations.Book
+{
+    public class BookCreateRequestValidator : AbstractValidator<BookCreateRequest>
+    {
+        public BookCreateRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+
+            RuleFor(x => x.Price)
+                .InclusiveBetween(5, 100)
+                .WithMessage("Price must be between 5 and 100");
+        }
+    }
+}
---
+++ JwtBasedAuthenticationAndAuthorization/DataValidations/Book/BookUpdateRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using JwtBasedAuthenticationAndAuthorization.Payloads.Book;
+
+namespace JwtBasedAuthenticationAndAuthorization.DataValidations.Book
+{
+    public class BookUpdateRequestValidator : AbstractValidator<BookUpdateRequest>
+    {
+        public BookUpdateRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+
+            RuleFor(x => x.Price)
+                .InclusiveBetween(5, 100)
+                .WithMessage("Price must be between 5 and 100");
+        }
+    }
+}
