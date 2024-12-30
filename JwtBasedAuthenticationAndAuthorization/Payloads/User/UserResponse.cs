351109b3f38c54a6e2ea97ba712d54a14a9545f6
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 11:20:53 +0400

    Create payload: UserResponse

---
+++ JwtBasedAuthenticationAndAuthorization/Payloads/User/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace JwtBasedAuthenticationAndAuthorization.Payloads.User
+{
+    public class UserResponse
+    {
+        public string Message { get; set; } = string.Empty;
+        public long Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime? BirthDateTime { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public List<string> Roles { get; set; } = new();
+        public DateTime? ModifiedDateTime { get; set; }
+    }
+}
