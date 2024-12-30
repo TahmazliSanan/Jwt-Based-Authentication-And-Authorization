40054da9df27b14edda020dbbe22bf7b1c5ba5d1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 10:25:42 +0400

    Create payload: UserRegisterRequest

---
+++ JwtBasedAuthenticationAndAuthorization/Payloads/User/UserRegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtBasedAuthenticationAndAuthorization.Payloads.User
+{
+    public class UserRegisterRequest
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime? BirthDateTime { get; set; }
+    }
+}
