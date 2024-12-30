f4cd0c48843426104e1e6bcb2b5092ce13ed21bb
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-30 12:34:04 +0400

    Create payload: LoginResponse

---
+++ JwtBasedAuthenticationAndAuthorization/Payloads/Login/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtBasedAuthenticationAndAuthorization.Payloads.Login
+{
+    public class LoginResponse
+    {
+        public string Message { get; set; } = string.Empty;
+        public long UserId { get; set; }
+        public string Jwt { get; set; } = string.Empty;
+    }
+}
